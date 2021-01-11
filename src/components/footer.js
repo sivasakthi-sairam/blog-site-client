ad37ef09e5a2e98159775674f9494d6ca27e241a
sivasakthi_sairam <sivasakthisairam@gmail.com>
2021-01-11 13:48:13 +0530

    client

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">@Protrainy</span>
+      </div>
+    </footer>
+  );
+}
